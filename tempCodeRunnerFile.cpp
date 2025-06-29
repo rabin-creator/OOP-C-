8a8c9f1af7bdadafa48e080bbadf62188ea9db68
rabin-creator <rabindc77@gmail.com>
2025-06-30 00:15:49 +0545

    banking deposit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,71 +1,43 @@
 #include <iostream>
 using namespace std;
 
-class house {
-public:
-    enum model { rock, brick, wood };
-
+class BankAccount {
 private:
-    int room_no;
-    string address;
-    model house_model;
+    // Private member variable
+    double balance;
 
 public:
-    void input() {
-        cout << "Enter room number: ";
-        cin >> room_no;
-        cout << "Enter address: ";
-        cin.ignore();
-        getline(cin, address);
-        cout << "Enter house model (rock/brick/wood): ";
-        string model_str;
-        cin >> model_str;
-        if (model_str == "rock")
-            house_model = rock;
-        else if (model_str == "brick")
-            house_model = brick;
-        else
-            house_model = wood;
+    // Constructor to initialize balance
+    BankAccount(double initialBalance) {
+        balance = initialBalance;
     }
 
-    void show() {
-        cout << "\n==============================\n";
-        cout << "      HOUSE DETAILS\n";
-        cout << "==============================\n";
-        cout << "Room No   : " << room_no << endl;
-        cout << "Address   : " << address << endl;
-        cout << "Model     : ";
-        switch (house_model) {
-            case rock: cout << "Rock"; break;
-            case brick: cout << "Brick"; break;
-            case wood: cout << "Wood"; break;
+    // Public member function to deposit money
+    void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            cout << "Deposited: " << amount << endl;
+        } else {
+            cout << "Invalid deposit amount!" << endl;
         }
-        cout << endl;
-        cout << "------------------------------\n";
-        show_safety();
-        cout << "==============================\n";
     }
 
-private:
-    void show_safety() {
-        cout << "Safety Measures: ";
-        switch (house_model) {
-            case rock:
-                cout << "Medium safety. Ensure regular inspection for cracks." << endl;
-                break;
-            case brick:
-                cout << "High safety. Good resistance to fire and weather." << endl;
-                break;
-            case wood:
-                cout << "Low safety. Take precautions against fire and termites." << endl;
-                break;
-        }
+    // Public member function to check balance
+    void checkBalance() {
+        cout << "Current balance: " << balance << endl;
     }
 };
 
 int main() {
-    house h1;
-    h1.input();
-    h1.show();
+    BankAccount myAccount(1000.0); // Create an object of BankAccount
+    double amount;
+    cout << "enter amount to deposit :";
+    cin >> amount;
+    myAccount.deposit(amount); // Deposit money
+    myAccount.checkBalance(); // Check balance
+
+    // The following line would cause a compilation error because balance is private
+    // cout << "Balance: " << myAccount.balance << endl;
+
     return 0;
 }
